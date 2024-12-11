a361366ee186133b311ef3e3091f34481fc901bb
chinmaya.behura <chinmaya.behura@i-exceed.com>
2024-12-11 18:57:09 +0530

    2nd of 11th Dec

--- starter-template/src/app/page.tsx
+++ starter-template/src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Header } from "@/sections/Header";
+import { HeroSection } from "@/sections/Hero";
+
+
+export { Header } from "@/sections/Header";
 export default function Home() {
   return (
     <div>
-      <h1>Home page</h1>
+      <Header />
+      <HeroSection />
      </div>
   );
 }
--- starter-template/src/sections/Hero.tsx
+++ starter-template/src/sections/Hero.tsx
@@ -1,3 +1,17 @@
+import memojiImage from "@/assets/images/memoji-computer.png";
+import Image from "next/image";
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div>
+      <div className="container">
+        <Image src={memojiImage} alt="Person peeking behind the laptop" />
+        <div>
+          <div></div>
+          <div>Available for new projects</div>
+        </div>
+        <h1>Building exception web apps using JAVA, Springboot</h1>
+        <p>I'm specialise in Java Backend Developer , high-performing , have hands on experties in Jenkin, Oracle & Springboot</p>
+      </div>
+    </div>
+  );
 };
